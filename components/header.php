96fd80154932b9fd296b04f1afa6ec5914dd26e5
charlie.khoa <charlie.khoa@360f.com>
2026-01-20 22:59:08 +0700

    update: role permission

--- components/header.php
+++ components/header.php
@@ -213,7 +213,7 @@ $avatarImagePath = $isLoggedIn ? getAvatarImagePath($userGioiTinh, $basePath) : 
                                     <span>Đơn hàng của tôi</span>
                                 </a>
                                 <?php endif; ?>
-                                <?php if ($showManagement): ?>
+                                <?php if ($isAdmin): ?>
                                 <a href="<?php echo $basePath; ?>pages/management/product-management.php" class="dropdown-item">
                                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                         <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
